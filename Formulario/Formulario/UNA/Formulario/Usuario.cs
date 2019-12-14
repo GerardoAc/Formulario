eae40821f30dc16da1f512877ac7b6d7e23627ce
ronald0309 <ronaldba16@gmail.com>
2019-12-14 09:09:53 -0600

    se crea la clase usuario

---
+++ Formulario/Formulario/UNA/Formulario/Usuario.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Formulario
+{
+    class Usuario
+    {
+        public Usuario()
+        {
+
+        }
+        public Usuario(string nombre,string contrasenna)
+        {
+            this.Nombre = nombre;
+            this.Contrasenna = contrasenna;
+        }
+        public string Nombre
+        {
+            get;
+            set;
+        }
+        public string Contrasenna
+        {
+            get;
+            set;
+        }
+        public string Validacion
+        {
+            get;
+            set;
+        }
+    }
+}
